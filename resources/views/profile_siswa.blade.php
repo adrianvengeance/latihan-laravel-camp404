59ac3b196a7f289f962b22c56ab9b1f06834a8ba
adrianvengeance <moh.adrian11@gmail.com>
2022-12-24 21:49:18 +0700

    CRUD siswa

---
+++ resources/views/profile_siswa.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="containter d-flex justify-content-center">
+    <div class="card w-50">
+      <div class="card-header">
+        <h3>Profile Siswa</h3>
+      </div>
+      <div class="card-body">
+        <a href="{{ route('siswa.index') }}"
+           class="mb-2">Kembali</a>
+        <div class="row ms-2">
+          <h4 class="col-4">Nama</h4>
+          <h4 class="col-8">: {{ $siswa->nama }}</h4>
+        </div>
+        <div class="row ms-2">
+          <h4 class="col-4">NIS</h4>
+          <h4 class="col-8">: {{ $siswa->nis }}</h4>
+        </div>
+        <div class="row ms-2">
+          <h4 class="col-4">Tanggal Lahir</h4>
+          <h4 class="col-8">: {{ $siswa->tgl_lahir }}</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
